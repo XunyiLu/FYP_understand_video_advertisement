cb79943e9e69f83d1c2b4c974b87cf4da154ea13
Procheta Sen <prochetasen@Prochetas-MacBook-Pro.local>
2023-03-03 16:07:24 +0000

    Feature Extraction Code Updated

--- feature_Extraction.py
+++ feature_Extraction.py
@@ -1,35 +1,116 @@
-import numpy as np
+from tensorflow import keras
 import tensorflow as tf
-from keras import backend as K
-from keras.applications.inception_v3 import InceptionV3
-from keras.models import Model
+import pandas as pd
+import numpy as np
+import os
 import cv2
 
+IMG_SIZE = 224
+BATCH_SIZE = 64
+EPOCHS = 10
 
-def get_model():
-    K.clear_session
-    inception_base = InceptionV3(weights='imagenet', include_top=True)
-    return Model(inputs = inception_base.input,
-                 outputs = inception_base.get_layer('avg_pool').output)
+MAX_SEQ_LENGTH = 20
+NUM_FEATURES = 2048
 
-if __name__ == '__main__':
-    # Step 1: Convert picture dimension to (299,299,3)
-    image_path = '/Users/lxy/Desktop/1.jpg'
-    new_image_path = '/Users/lxy/Desktop/3.jpg'
-    img = cv2.imread(image_path) # Read BGR of the picture
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert BGR to RGB
-    img4 = cv2.resize(img,(299,299))
-    cv2.imwrite(new_image_path, img4)
+root_path="/Users/prochetasen/Downloads/features/"
+train_df = pd.read_csv("/Users/prochetasen/Desktop/train.csv")
+train_df.sample(10)
 
-    # Step 2: Build the model
-    model = get_model()
+def crop_center_square(frame):
+    y, x = frame.shape[0:2]
+    min_dim = min(y, x)
+    start_x = (x // 2) - (min_dim // 2)
+    start_y = (y // 2) - (min_dim // 2)
+    return frame[start_y : start_y + min_dim, start_x : start_x + min_dim]
 
-    # Step 3: Extract features
-    image = tf.keras.utils.load_img(new_image_path, target_size=None)
-    input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    input_arr = np.array([input_arr])  # Convert single image to a batch.
-    predictions = model.predict(input_arr)
+def load_video(path, max_frames=0, resize=(IMG_SIZE, IMG_SIZE)):
+    cap = cv2.VideoCapture(path)
+    frames = []
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = crop_center_square(frame)
+            frame = cv2.resize(frame, resize)
+            frame = frame[:, :, [2, 1, 0]]
+            frames.append(frame)
 
-    # Step 4: Store the features
-    feature_save_path = "/Users/lxy/Desktop/11111111"
-    np.save(feature_save_path, predictions)
+            if len(frames) == max_frames:
+                break
+    finally:
+        cap.release()
+    return np.array(frames)
+
+def build_feature_extractor():
+    feature_extractor = keras.applications.InceptionV3(
+        weights="imagenet",
+        include_top=False,
+        pooling="avg",
+        input_shape=(IMG_SIZE, IMG_SIZE, 3),
+    )
+    preprocess_input = keras.applications.inception_v3.preprocess_input
+
+    inputs = keras.Input((IMG_SIZE, IMG_SIZE, 3))
+    preprocessed = preprocess_input(inputs)
+
+    outputs = feature_extractor(preprocessed)
+    return keras.Model(inputs, outputs, name="feature_extractor")
+
+
+feature_extractor = build_feature_extractor()
+label_processor = keras.layers.StringLookup(
+    num_oov_indices=0, vocabulary=np.unique(train_df["label"])
+)
+print(label_processor.get_vocabulary())
+
+def prepare_all_videos(df, root_dir):
+    num_samples = len(df)
+    video_paths = df["video_name"].values.tolist()
+    labels = df["label"].values
+    labels = label_processor(labels[..., None]).numpy()
+
+    vs=[]
+    vs=os.listdir("/Users/prochetasen/Downloads/videos/")
+
+    # `frame_masks` and `frame_features` are what we will feed to our sequence model.
+    # `frame_masks` will contain a bunch of booleans denoting if a timestep is
+    # masked with padding or not.
+    frame_masks = np.zeros(shape=(num_samples, MAX_SEQ_LENGTH), dtype="bool")
+    frame_features = np.zeros(
+        shape=(num_samples, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32"
+    )
+
+    # For each video.
+    for idx, path in enumerate(vs):
+        # Gather all its frames and add a batch dimension.
+        frames = load_video(os.path.join(root_dir, path))
+        frames = frames[None, ...]
+
+        # Initialize placeholders to store the masks and features of the current video.
+        temp_frame_mask = np.zeros(shape=(1, MAX_SEQ_LENGTH,), dtype="bool")
+        temp_frame_features = np.zeros(
+            shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32"
+        )
+
+        # Extract features from the frames of the current video.
+        for i, batch in enumerate(frames):
+            video_length = batch.shape[0]
+            length = min(MAX_SEQ_LENGTH, video_length)
+            for j in range(length):
+                temp_frame_features[i, j, :] = feature_extractor.predict(
+                    batch[None, j, :]
+                )
+            temp_frame_mask[i, :length] = 1  # 1 = not masked, 0 = masked
+        name_vs=path.split("###")[1].split(".mp4")[0].strip()
+        name_vs=name_vs.replace("?","")
+        np.save(root_path+name_vs+"_mask.npy",temp_frame_mask.squeeze())
+        np.save(root_path+name_vs+"_feature.npy",temp_frame_features.squeeze())
+        frame_features[idx,] = temp_frame_features.squeeze()
+        frame_masks[idx,] = temp_frame_mask.squeeze()
+
+    return (frame_features, frame_masks), labels
+
+
+x,y=prepare_all_videos(train_df, "/Users/prochetasen/Downloads/videos/")
+print(len(x))
