80a619026dca06392eacc0545d6eca73aa32f63d
Xunyi Lu <96417787+codeGodlxy@users.noreply.github.com>
2023-03-14 17:56:37 +0000

    Update download_Youtube_videos.py

--- download_Youtube_videos.py
+++ download_Youtube_videos.py
@@ -1,31 +1,26 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from pytube import YouTube
 
 
-def print_hi(name):
-    print('Hi', name)  # Press ⌘F8 to toggle the breakpoint.
+def download_youtube_videos(csv, target_path):
+    # data = ' '
+    with open(csv, 'r') as f:
+        for line in f.readlines():
+            line = line.replace("'", "")
+            data = 'https://www.youtube.com/watch?v=' + line
+            print(data)
+            yt = YouTube(data)
+
+            try:
+                # print(yt.)
+                yt.streams.get_highest_resolution().download(target_path, filename=line + '.mp4')
+                # video.download('/User/lxy/Documents/')
+                # print(data)
+
+            except:
+                print("An exception occurred")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    from pytube import YouTube
-    data = ' '
-    with open('/Users/lxy/Desktop/FYP/final_video_id_list.csv', 'r') as f:
-            for line in f.readlines():
-                line = line.replace("'","")
-                data = 'https://www.youtube.com/watch?v=' + line
-                print(data)
-                yt = YouTube(data)
+download_youtube_videos("/Users/bunnylu/Desktop/FYP/final_video_id_list_01.csv",
+                        "/Users/bunnylu/Desktop/FYP/database/video/")
 
-                try:
-                    # print(yt.)
-                    yt.streams.get_highest_resolution().download('/Users/lxy/Desktop/FYP/videos/new/', filename = "_v_exciting_"+ line + '.mp4')
-                    # video.download('/User/lxy/Documents/')
-                    # print(data)
-
-
-                except:
-                    print("An exception occurred")
 
